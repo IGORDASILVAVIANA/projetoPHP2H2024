c1de872c80cd080e3ba2c99e22cd2bb03f560138
IGORDASILVAVIANA <igorvianna206@gmail.com>
2024-08-12 19:57:38 -0300

    quarto exercício

---
+++ quarto.php
@@ -0,0 +1,20 @@
+<?php 
+	
+	$data = "12 de agosto de 2024";
+	$salario = 850.00;
+	$idade = 18;
+	$cargo = "Estágiario";
+	$resultado = true;
+
+
+	echo "Data $data <br />";
+	echo "Salário $salario <br />";
+	echo "idade $idade <br />";
+	echo "cargo $cargo <br />";
+
+	if ($resultado) {
+		echo "Resultado é verdadeiro.";
+	}
+
+
+ ?>
