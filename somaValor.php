e2b766a1f28e6d3329f141d0f69532812440fa43
IGORDASILVAVIANA <igorvianna206@gmail.com>
2024-08-14 22:07:39 -0300

    Estrutura de decisão

---
+++ somaValor.php
@@ -0,0 +1,19 @@
+<?php 
+
+	$num1 = 20.50;
+	$num2 = 10.15;
+	$resp = 0.0;
+
+	$resp = $num1 + $num2;
+
+	if ($resp <= $num1) {
+		
+		echo "Resposta é maior do que o número 1";
+	}else{
+
+		echo "Resposta é menor do que o número 1 ";
+	}
+
+
+ ?>
+
